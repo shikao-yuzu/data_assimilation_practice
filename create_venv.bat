ace63a7364a5c00409f6b74976c1e619ae329821
shikao-yuzu <shikao.yuzu@gmail.com>
2020-02-19 16:17:19 +0900

    ...

--- create_venv.bat
+++ create_venv.bat
@@ -13,9 +13,9 @@ rem install dependency package
 python -m pip install --upgrade pip
 python -m pip install --upgrade setuptools
 
-pip install numpy==1.18.0
-pip install pandas==0.25.3
-pip install matplotlib==3.1.2
+pip install numpy==1.18.1
+pip install pandas==1.0.1
+pip install matplotlib==3.1.3
 pip install japanize-matplotlib==1.0.5
 
 rem install this package
